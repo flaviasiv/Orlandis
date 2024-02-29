ce00e09c40b78043aabf3c830ffefbd2ad0c9fdc
flavia sigoli <flaviasivm@gmail.com>
2024-02-29 12:08:38 -0300

    alteracoes1 29-02

--- js/javascript.js
+++ js/javascript.js
@@ -12,41 +12,6 @@ document.onclick = function(e){
         box.classList.remove("active_box");
     }
 }
-// scroll navbar
-const navbar = document.getElementById("homenav");
-
-            let lastScrollTop = 0;
-            addEventListener("scroll", () => {
-                // Current scroll position
-                const scrollTop =
-                    window.pageYOffset || document.documentElement.scrollTop;
-
-                // check scroll direction
-                const distance = scrollTop - lastScrollTop;
-                const currentTop = parseInt(
-                    getComputedStyle(navbar).top.split("px")
-                );
-
-                // Clamp value between -80 and 0
-                let amount = Math.max(
-                    Math.min(
-                        currentTop +
-                            (distance < 0
-                                ? Math.abs(distance) // scrolling up
-                                : -Math.abs(distance)// scrolling down
-                                ) * 40, // (1)
-                        0
-                    ),
-                    -80
-                );
-
-                console.log(amount, currentTop, Math.abs(distance));
-
-                navbar.style.top = `${amount}px`;
-
-                lastScrollTop = scrollTop;
-            });
-
 
 // scroll behavior
 function reveal() {
